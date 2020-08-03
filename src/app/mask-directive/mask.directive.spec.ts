6d4885809d5838764cc44ebdf4e9931249834840
CHIRICO ANDREA <andrea.chirico@P-ACHIRICO.dgsgroup.it>
2020-08-03 17:33:19 +0200

    arrow up test added

--- src/app/mask-directive/mask.directive.spec.ts
+++ src/app/mask-directive/mask.directive.spec.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
-import { Separators, MaskConfigOptions } from './mask-options';
+import { Separators, MaskConfigOptions, ArrowBehaviours } from './mask-options';
 import { MaskDirective } from './mask.directive';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
@@ -24,7 +24,8 @@ class TestComponent {
     constructor() {
         this.formControl = new FormControl('2019-10-10T00:59:00');
         this.maskConfigOptions = {
-            dateConfiguration: 'ddMMyy'
+            dateConfiguration: 'ddMMyy',
+            arrowBehaviours: ArrowBehaviours.circular_without_position
         }
     }
 }
@@ -48,10 +49,9 @@ describe('mask-directive', () => {
 
       fixture = TestBed.createComponent(TestComponent);
       component = fixture.componentInstance;
-
       directiveElement = fixture.debugElement.query(By.directive(MaskDirective));
       directiveInstance = directiveElement.injector.get(MaskDirective)
-        inputElement = fixture.debugElement.query(By.css('input'));
+      inputElement = fixture.debugElement.query(By.css('input'));
       fixture.detectChanges();
     })
 
@@ -62,17 +62,34 @@ describe('mask-directive', () => {
       expect(directiveInstance["dateTimeSeparator"]).toBe(Separators.space);
     });
 
-    it("should change value correctly", async(() => {
+    // it("should change value correctly", async(() => {
+    //   fixture
+    //     .whenStable()
+    //     .then(() => {
+    //       inputElement.nativeElement.value = "11-10-2019";
+    //       inputElement.nativeElement.dispatchEvent(new Event("keypress"));
+    //       return fixture.detectChanges();
+    //     })
+    //     .then(() => {
+    //       expect().toBe("11-10-2019");
+          
+    //     });
+    // }));
+
+    it("should detect arrowUp value correctly", async(() => {
       fixture
-        .whenStable()
-        .then(() => {
-          inputElement.nativeElement.value = "11-10-2019";
-          inputElement.nativeElement.dispatchEvent(new Event("input"));
+      .whenStable()
+      .then(() => {
+        component.maskConfigOptions.arrowBehaviours = ArrowBehaviours.circular_without_position;
           return fixture.detectChanges();
         })
         .then(() => {
-          expect(inputElement.nativeElement.value).toBe("11-10-2019");
-          
+          inputElement.nativeElement.value = "11-10-2019";
+          inputElement.nativeElement.dispatchEvent(new KeyboardEvent('keydown',{key: 'ArrowUp'} ));
+          return fixture.detectChanges();
+        })
+        .then(() => {
+          expect(inputElement.nativeElement.value).toBe("12-10-2019");
         });
     }));
 
