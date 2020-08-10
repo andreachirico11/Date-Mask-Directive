3f0e598704754ea74e35eefe21bec60a9a0ee4b0
CHIRICO ANDREA <andrea.chirico@P-ACHIRICO.dgsgroup.it>
2020-08-10 11:05:47 +0200

    view completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { FormControl } from "@angular/forms";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
   maskConfigOptions: MaskConfigOptions;
